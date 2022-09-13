f3ac633a75eeb2d9a884a48f6c4a642f9d6f29f6
Dario Plácido Gandini <darioplacidogandini@outlook.com>
2022-09-13 19:11:22 -0300

    .

--- queries.js
+++ queries.js
@@ -18,6 +18,17 @@ const getBooks = (request,response) => {
     })
 }
 
+const addBook = (request, response) => {
+    const {name, email} = request.body
+
+    pool.query('INSER INTO users (title, author) VALUES ($1, $2)'), 
+    [name, email], (error, results) => {
+        if (error) {
+            throw error
+        }
+    }) 
+}
+
 module.exports = {
     getBooks,
 }
